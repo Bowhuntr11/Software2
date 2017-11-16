97eaa7fff24c778e2456d495a8e313d5c5b70341
Blake Sherrill <bowhuntr11@hotmail.com>
2017-11-16 08:25:24 -0600

    fixedcomboBoxes

--- src/software2/NewAppointmentController.java
+++ src/software2/NewAppointmentController.java
@@ -6,11 +6,13 @@
 package software2;
 
 import java.net.URL;
-import java.time.LocalTime;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Spinner;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -18,12 +20,55 @@ import javafx.scene.control.Spinner;
  * @author SFFPC
  */
 public class NewAppointmentController implements Initializable {
+
     @FXML
-    private Spinner startTime;
+    private TextField title;
+    @FXML
+    private TextField description;
+    @FXML
+    private TextField location;
+    @FXML
+    private TextField url;
+    @FXML
+    private ComboBox selectedCustomer;
+    @FXML
+    private Button save;
+    @FXML
+    private Button cancel;
+    @FXML
+    private ComboBox startMin;
+    @FXML
+    private ComboBox startHour;
+    @FXML
+    private ComboBox startAMPM;
+    @FXML
+    private ComboBox endMin;
+    @FXML
+    private ComboBox endHour;
+    @FXML
+    private ComboBox endAMPM;
     
+    private final String[] hours = {"1","2","3","4","5","6","7","8",
+                                        "9","10","11","12"};
+    private final String[] mins = {"00","05","10","15","20","25","30",
+                                        "35","40","45","50","55"};
+    private final String[] ampm = {"AM","PM"};
+
+    /**
+     * Initializes the controller class.
+     */
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        startTime = new SpinnerTime();
+    public void initialize(URL url, ResourceBundle rb) {
+        setData();
+    }    
+    
+    public void setData(){
+        
+        startHour.setItems(FXCollections.observableArrayList(hours));
+        startMin.setItems(FXCollections.observableArrayList(mins));
+        endHour.setItems(FXCollections.observableArrayList(hours));
+        endMin.setItems(FXCollections.observableArrayList(mins));
+        startAMPM.setItems(FXCollections.observableArrayList(ampm));
+        endAMPM.setItems(FXCollections.observableArrayList(ampm));
     }
 }
- 
